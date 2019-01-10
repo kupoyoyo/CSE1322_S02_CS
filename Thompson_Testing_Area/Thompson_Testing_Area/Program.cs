87c9d8e6b820c36a88f757410ea1e86301cacc49
SomeLog <loganthompson2011@gmail.com>
2019-01-09 19:24:49 -0500

    Created Thompson_Testing_Area
    Created a C# console application for testing code

---
+++ Thompson_Testing_Area/Thompson_Testing_Area/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson_Testing_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello class \nWelcome to CSE1322!";
+            Console.WriteLine(hello);
+        }
+    }
+}
