713c0fef655d758c5f57b251f9d883ef37a62c14
SomeLog <loganthompson2011@gmail.com>
2019-01-11 13:05:50 -0500

    Changed Testing Area Code
    Followed class lecture

---
+++ Thompson_Testing_Area/Thompson_Testing_Area/TestingProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson_Testing_Area
+{
+    public class TestingProgram
+    {
+        public static void Main(string[] args)
+        {
+            string hello = "Hello class\nWelcome to CSE1322!";
+            Console.WriteLine(hello);
+
+            char ch = 'A';
+            //char ch = 65; will not work in C#
+            Console.WriteLine(ch);
+
+            int x = 4; //Uses 4 bytes to store the number
+            byte b = 127; //Uses 1 byte in memory to store number. Used for short numbers. (-128 to 127)
+            short s = 3412; //Uses 2 bytes
+            long l = 45345; //Uses 8 bytes. Only for very large numbers
+            Console.WriteLine(x);
+            Console.WriteLine(b);
+            Console.WriteLine(s);
+            Console.WriteLine(l);
+
+            double d = 23.50; //Uses 8 bytes
+            float f = 45.50f; //Uses 4 bytes. Requires f after the number
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+
+            bool bl = true;
+            Console.WriteLine(!bl);
+
+
+        }
+    }
+}
