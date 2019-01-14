b357e5ffa993dcac2ac39da35ccb438ecee2b408
SomeLog <loganthompson2011@gmail.com>
2019-01-14 14:45:48 -0500

    Added code to Assignment1B

--- Thompson_Assignment1B/Thompson_Assignment1B/Program.cs
+++ Thompson_Assignment1B/Thompson_Assignment1B/Program.cs
@@ -10,6 +10,19 @@ namespace Thompson_Assignment1B
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This application will compute the test score average and grade for each student");
+
+
+
+
+            string run = "y";
+            do
+            {
+                Console.Write("First Name: ");
+                
+                Console.Write("Continue inputting students? y/n ");
+                run = Console.ReadLine();
+            } while (run == "y");
         }
     }
 }
--- Thompson_Assignment1B/Thompson_Assignment1B/Test.cs
+++ Thompson_Assignment1B/Thompson_Assignment1B/Test.cs
@@ -2,8 +2,56 @@
 
 public class Test
 {
-	public Test()
-	{
+    private string firstName, lastName;
+    private int[] test = new int[5];
+    private int testAvg;
+    private char grade;
 
+    public string FirstName
+    {
+        get { return firstName; }
+    }
+    public string LastName
+    {
+        get { return lastName; }
+    }
+    public int[] Test
+    {
+        get { return test; }
+    }
+    public int TestAvg
+    {
+        get { return testAvg; }
+    }
+    public char Grade
+    {
+        get { return grade; }
+    }
+
+    public Test (string fn, string ln, int tst, int tstavg, char grd)
+    {
+        FirstName = fn;
+        LastName = ln;
+        Test = tst;
+        TestAvg = tstavg;
+        Grade = grd;
+    }
+
+    public Test()
+	{
+        FirstName = "Steve";
+        LastName = "Stevenson";
+        Test = 0; //Need to edit a proper array value
+        Grade = 'F';
 	}
+
+    public ComputeAverage (int tst)
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return FirstName + " " + LastName + " " + Test[0] + " " + Test[1] + " " + Test[2] + " " + Test[3] + " " + Test[4] + " " + TestAvg + " " + Grade;
+    }
 }
