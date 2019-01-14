e5dfcae46e0ea8b70c1aff4b05cf424cb7664cc3
SomeLog <loganthompson2011@gmail.com>
2019-01-14 13:08:16 -0500

    Update Program.cs

--- Thompson_Lab1A/Thompson_Lab1A/Program.cs
+++ Thompson_Lab1A/Thompson_Lab1A/Program.cs
@@ -10,6 +10,16 @@ namespace Thompson_Lab1A
     {
         static void Main(string[] args)
         {
+            int frequency, duration; //declaring variables
+
+            Console.WriteLine("Make sure your volume is on to hear the beep!");
+            Console.Write("Please enter a frequency: ");
+            frequency = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a duration (ms): ");
+            duration = Int32.Parse(Console.ReadLine());
+
+            Console.Beep(frequency, duration); //creates a computer beep using inputted frquency and duration
         }
     }
 }
