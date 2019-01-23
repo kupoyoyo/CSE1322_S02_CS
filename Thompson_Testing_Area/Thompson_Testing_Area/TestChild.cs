a4ba21051e37ae884aecceda099e2758a43d25d0
Logan Thompson <loganthompson2011@gmail.com>
2019-01-23 14:51:14 -0500

    Reviewed Inheritance and Polymorphism

---
+++ Thompson_Testing_Area/Thompson_Testing_Area/TestChild.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson_Testing_Area
+{
+    class TestChild : TestParent
+    {
+        public override int someMethod(int x)
+        {
+            return x+ 45;
+        }
+    }
+}
---
+++ Thompson_Testing_Area/Thompson_Testing_Area/TestParent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson_Testing_Area
+{
+    class TestParent
+    {
+        public int somenumber = 6;
+
+
+        public TestParent ()
+        {
+
+        }
+
+        public virtual int someMethod (int x)
+        {
+            return x + 5;
+        }
+    }
+
+}
