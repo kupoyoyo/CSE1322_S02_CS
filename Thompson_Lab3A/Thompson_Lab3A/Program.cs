52882719ae7fb3f467c7893c3c4a1a6a098beb31
Logan Thompson <loganthompson2011@gmail.com>
2019-02-08 13:10:31 -0500

    Lab 3A Initial Build
    Still needs editting to better fit lab requirements.

---
+++ Thompson_Lab3A/Thompson_Lab3A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thompson_Lab3A
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Thompson_Lab3A_Form());
+        }
+    }
+}
---
+++ Thompson_Lab3A/Thompson_Lab3A/Thompson_Lab3A_Form.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thompson_Lab3A
+{
+    public partial class Thompson_Lab3A_Form : Form
+    {
+        public Thompson_Lab3A_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 1 was clicked");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 2 was clicked");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 3 was clicked");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 4 was clicked");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 5 was clicked");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button 6 was clicked");
+        }
+
+        private void Thompson_Lab3A_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
