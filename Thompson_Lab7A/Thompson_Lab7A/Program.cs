15dd66ce16c0622bc69b43aaef522d07ca1aa271
Logan Thompson <loganthompson2011@gmail.com>
2019-04-08 14:01:39 -0400

    Lab 7A Complete

---
+++ Thompson_Lab7A/Thompson_Lab7A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thompson_Lab7A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 10;
+            int sumParallel = 0;
+
+            int[] array = new int[100000];
+
+            Random randNum = new Random();
+
+            Parallel.ForEach(array, i =>
+            {
+                array[i] = randNum.Next(min, max);
+                sumParallel += array[i];
+            }
+            );
+
+            Console.WriteLine("Sum via multithreading: " + sumParallel);
+        }
+    }
+}
